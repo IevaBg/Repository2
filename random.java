10399cca6a61a09c9b6a98c4ba9673160db49644
IevaBg <86737962+IevaBg@users.noreply.github.com>
2021-07-07 21:52:35 +0300

    Update random.java

--- random.java
+++ random.java
@@ -1,1 +1,13 @@
+public class random {
 
+    public static void main (String[] args) {
+
+        int a = -5 + 8 * 6;
+        int b = (55+9) % 9;
+        int c = 5 + 15 / 3 * 2 - 8 % 3;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
